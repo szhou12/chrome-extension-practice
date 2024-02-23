f8dbbc7de4784b0c5f92a6284ae35320106cd8a8
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2024-02-22 19:44:08 -0600

    add getting started stage

---
+++ extension/content.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello, world!');
+/******/ })()
+;
+//# sourceMappingURL=content.js.map
